cc447e77a64db50e8bb41a961ba5743ac9194516
YOGESH <165576853+yogeshrwat@users.noreply.github.com>
2024-04-10 21:42:03 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,19 +30,19 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-
+    
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_bhbjivb',
+        'template_ng422hj',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Yogesh",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "yogeshrawatdev@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'HLJobgWVCl9sZseyk'
       )
       .then(
         () => {
